6a2d00ae40a793576907e1146167c392130d8893
minhazsami <minhazsami@yahoo.com>
2022-12-19 14:57:17 +0600

    trying to visit the webpage

---
+++ cypress/e2e/POM/signUP.cy.js
@@ -0,0 +1,5 @@
+class signUp {
+
+}
+
+export default signUp
---
+++ cypress/e2e/e2eTest.spec.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+import signUp from "./POM/signUP.cy";
+const singup = new signUp();
+
+describe('My First Test', () => {
+    let data; //closure variable
+
+    before(() => {
+        // executes once prior all tests in it block
+        cy.fixture('credential').then(function (data) {
+            this.data = data
+        });
+    });
+    it('Verify the HomePage', () => {
+
+        cy.visit('https://shopthearea.netlify.app/')
+    })
+})
